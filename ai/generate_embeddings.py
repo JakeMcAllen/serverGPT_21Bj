49ebfe38618c484ce32fe25c684727dceb47d80f
21BJ <hello@21bj.io>
2024-09-09 11:44:16 +0200

    update and correct same bugs

--- ai/generate_embeddings.py
+++ ai/generate_embeddings.py
@@ -14,6 +14,6 @@ text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=10
 docs = text_splitter.split_documents(docs)
 embedding_function = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2", model_kwargs={'device': "cpu"})
 
-vectorstore = Chroma.from_documents(docs, embedding_function, persist_directory="./choma_db_nccn")
+vectorstore = Chroma.from_documents(docs, embedding_function, persist_directory="./chroma_db_nccn")
 
 print(vectorstore._collection.count())
