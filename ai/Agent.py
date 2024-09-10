8e1707fc06604a047617fad50a4380c62fe92673
21BJ <hello@21bj.io>
2024-09-10 08:54:37 +0200

    Update methods for call gemini ai and routine for ai

--- ai/Agent.py
+++ ai/Agent.py
@@ -3,40 +3,38 @@ import nltk
 
 import requests 
 
+import google.generativeai as genai
+
+from rag import get_relevant_context_from_db, generate_prompt
+
+
+
+
 
 nltk.download('wordnet')
 
-extraInputInfo =  """You are a bot that makes recommendations for activities. You answer in very short sentences and do not include extra information.
-      This is the recommended activity:
-      {relevant_document}
-      The user input is:
-      {user_input}
-      Use the contect words:  
-      {contextWords}
-      Compile a recommendation to the user based on the recommended activity and the user input."""
 
-API_URL = "https://api-inference.huggingface.co/models/openai-community/gpt2"
-API_key = "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"
+# API_URL = "https://api-inference.huggingface.co/models/openai-community/gpt2"
+# API_key = "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"
+GEMINI_API_KEY = "AIzaSyCFhiseeAtrOTwRc3Ib0-h4vqFha7KLgPM"
 
 
 
-def Agent(question, context):
+def Agent(query, keyWords):
         # WordNet
-        context = [wn.synsets(word)[0].name().split(".")[0] if wn.synsets(word) != [] else '' for word, qnt in context.items()]
-        context.remove('')
-        contextList = ", ".join(context) 
+        keyWords = [wn.synsets(word)[0].name().split(".")[0] if wn.synsets(word) != [] else '' for word, qnt in keyWords.items()]
+        keyWords.remove('')
+        contextList = ", ".join(keyWords) 
 
         # RAG
-        """TODO"""
-        docs = ""
-
+        docs = get_relevant_context_from_db(query)
 
         # Build response
-        question = extraInputInfo.format(relevant_document=docs ,user_input=question, contextWords=contextList)
-
+        question = generate_prompt(query, docs, contextList)
+        print(f"\n\nQuestion: {question}\n\n")
 
         # Question to Model
-        response = get_llama_RLHF_response(question)
+        response = get_gemini_response(question)
         # response = get_model_response(question)
 
         # Register data to DB
@@ -50,20 +48,36 @@ def get_model_response(msg: str):
     pass
 
 
+def get_gemini_response(prompt):
+    genai.configure(api_key=GEMINI_API_KEY)
+    model = genai.GenerativeModel(model_name='gemini-pro')
+    answere = model.generate_content(prompt)
+    return answere.text
 
-def get_llama_RLHF_response(msg: str, min_length=256, max_length=500, print_response=False, eliminate_input=True) -> str:
-    payload = { "inputs": msg, "top_k": max_length, "min_length": min_length, max_length:"max_length", "temperature": 0.8, "max_time": 40, "do_sample": True }
-    headers = {"Authorization": f"Bearer {API_key}"} 
-    errorCount = 0
+
+query = "How work blockchain ?"
+kw = {"blockchain": 1, "work": 2}
+
+resp = Agent(query, kw)
+print(f"\nresp: {resp}\n")
+
+
+# print( get_gemini_response("Introduce yourself please.") )
+
+# def get_llama_HF_response(msg: str, min_length=256, max_length=500, print_response=False, eliminate_input=True) -> str:
+#     payload = { "inputs": msg, "top_k": max_length, "min_length": min_length, max_length:"max_length", "temperature": 0.8, "max_time": 40, "do_sample": True }
+#     headers = {"Authorization": f"Bearer {API_key}"} 
+#     errorCount = 0
     
-    while True:
-        try:
-            response = requests.post(API_URL, headers=headers, json=payload)
+#     while True:
+#         try:
+#             response = requests.post(API_URL, headers=headers, json=payload)
             
-            if response.status_code != 200: raise Exception(f"response:{response} and status: {response.json()}")
-            if print_response: print(response.json(), end="\n\n")
-            return response.json()[0]["generated_text"][len(msg) + 1:] if eliminate_input else response.json()[0]["generated_text"]
+#             if response.status_code != 200: raise Exception(f"response:{response} and status: {response.json()}")
+#             if print_response: print(response.json(), end="\n\n")
+#             return response.json()[0]["generated_text"][len(msg) + 1:] if eliminate_input else response.json()[0]["generated_text"]
             
-        except Exception as e:
-            print(f"Error: {e}" if errorCount == 0 else "." if errorCount%100000 == 0 else "", end="")
-            errorCount += 1
+#         except Exception as e:
+#             print(f"Error: {e}" if errorCount == 0 else "." if errorCount%100000 == 0 else "", end="")
+#             errorCount += 1
+# get_gemini_RLHF_response(msg="Hello wold")
