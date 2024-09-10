5378bb91336355f5819f14807dfb3491416b6da5
21BJ <hello@21bj.io>
2024-09-10 08:59:37 +0200

    Low update

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from flask import Flask
+from ai.Agent import Agent
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/agent")
+def AgentCall(query, keyWords):
+    print(query)
+    print(keyWords)
+    return "true"
+
+    # return Agent(query, keyWords)
